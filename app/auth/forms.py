8a76798425b5c97ebb5f5dd4700a30b714f85d9a
sn0g9e <kung_schuh@hotmail.com>
2017-08-11 23:14:32 -0600

    changing to flask_wtf, adding psycopg2==2.7.3

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email
+
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),
