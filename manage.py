b97cf9292b5e4ad1525bdaba26676aa23bb9d044
sn0g9e <kung_schuh@hotmail.com>
2017-08-14 23:23:30 -0600

    html img tag; python shell

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ if os.path.exists('.env'):
             os.environ[var[0]] = var[1]
 
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
